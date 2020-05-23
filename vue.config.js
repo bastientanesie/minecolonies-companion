4f487925f45d46a66fc6581f472a0fc6e02d12ee
Bastien Tanésie <bastien.tanesie@gmail.com>
2020-05-23 15:14:33 +0200

    feat: change page title

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,12 @@ module.exports = {
         ? '//minecolonies-companion/'
         : '/',
     outputDir: 'dist',
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = require('./package.json').title;
+                return args;
+            })
+    },
 };
